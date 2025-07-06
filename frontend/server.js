3f2ac872ccb2a32d22c1438be39afe1d2f37038d
Janmejay Tiwari <okrtwr3108@gmail.com>
2025-07-06 21:29:11 +0530

    -H flag fix

--- frontend/server.js
+++ frontend/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files from build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Handle React routing - serve index.html for all routes
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
